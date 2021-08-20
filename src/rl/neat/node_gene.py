a9ce049c27547cf2f75860601bed4284b9490020
UltimateBobo <tomsha9@gmail.com>
2021-08-20 15:05:08 +0300

    removed index_in_layer attribute from NodeGene

--- src/rl/neat/node_gene.py
+++ src/rl/neat/node_gene.py
@@ -8,16 +8,15 @@ class NodeGene(Gene):
         - index_in_layer may need to be used for visualization only
     '''
 
-    def __init__(self, innovation_nb, connections=deque(), output=0, layer_nb=0, index_in_layer=0):
+    def __init__(self, innovation_nb, connections=deque(), output=0, layer_nb=0):
         super().__init__(innovation_nb)
         self.connections = connections
         self.output = output
         self.layer_nb = layer_nb
-        self.index_in_layer = index_in_layer
 
 
     def copy(self):
-        return NodeGene(self.innovation_nb, self.connections, self.output, self.layer_nb, self.index_in_layer)
+        return NodeGene(self.innovation_nb, self.connections, self.output, self.layer_nb)
 
 
     def __eq__(self, node_gene):
@@ -26,4 +25,4 @@ class NodeGene(Gene):
 
     def __str__(self):
         return f'NodeGene[innovation_nb={self.innovation_nb}, connections={self.connections}, \
-        output={self.output}, layer_nb={self.layer_nb}, index_in_layer={self.index_in_layer}]'
+        output={self.output}, layer_nb={self.layer_nb}]'
