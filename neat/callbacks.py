b5920c376aeb665c3fde57ccdcdaa625ae5c2de8
TheTompiler <Tomsha9@gmail.com>
2021-08-29 15:17:28 +0300

    added GenomeSavingCallback

--- neat/callbacks.py
+++ neat/callbacks.py
@@ -23,48 +23,4 @@ class GenomeSavingCallback():
             neat.genomes[idx].save(path=self.filepaths[idx] + f'-gen-{generation}.cfg')
 
 
-class GenerationTermination():
-    def __init__(self, stop_generation):
-        self.stop_generation = stop_generation
-
-
-    def __call__(self, neat, curr_generation):
-        return curr_generation < self.stop_generation
-
-
-class FitnessTermination():
-    def __init__(self, termination_fitness, top=1):
-        self.termination_fitness = termination_fitness
-        self.top = top
-    
-
-    def __call__(self, neat, sorted=False):
-        if sorted:
-            for idx in range(self.top):
-                if neat.genomes[idx].fitness < self.termination_fitness:
-                    return False
-        
-            return True
-        
-        count = 0
-
-        for genome in neat.genomes:
-            if genome.fitness >= self.termination_fitness:
-                count += 1
-
-                if count == self.top:
-                    return True
-        
-        return False
-
-
-class TimeTermination():
-    def __init__(self):
-        pass
-
-
-
-
-
-
         
