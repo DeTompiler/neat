6a1d9cbc51b45a1027f270b3669585aadd34bc0c
UltimateBobo <tomsha9@gmail.com>
2021-08-30 19:57:14 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+    name = "neatp",
+    version= "0.10",
+    author = "Tom S.",
+    author_email = "",
+    description = "NeuroEvolution of Augmenting Topologies",
+    long_description = "",
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/DeTompiler/Neat",
+    project_urls={
+        "Bug Tracker": "https://github.com/DeTompiler/Neat/issues",
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
