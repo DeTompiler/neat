2e046b996d8e4ce32fb0b61fc91275d939b9bede
UltimateBobo <tomsha9@gmail.com>
2021-08-31 17:22:59 +0300

    changed neat parameters

--- neat/neat.py
+++ neat/neat.py
@@ -38,13 +38,13 @@ class Neat:
         self.kill_worst = kwargs.get('kill_worst', 0.2)
 
         self.toggle_probability = kwargs.get('toggle_probability', 0.01)
-        self.shift_probability = kwargs.get('shift_probability', 0.01)
+        self.shift_probability = kwargs.get('shift_probability', 0.03)
         self.random_probability = kwargs.get('random_probability', 0.01)
         self.connection_probability = kwargs.get('connection_probability', 0.05)
         self.node_probability = kwargs.get('node_probability', 0.03)
 
         self.weight_randomization_factor = kwargs.get('weight_randomization_factor', 1.)
-        self.weight_shift_factor = kwargs.get('weight_shift_factor', 1.)
+        self.weight_shift_factor = kwargs.get('weight_shift_factor', 0.2)
         self.similar_fitness_range = kwargs.get('similar_fitness_range', 0.04)
         self.max_add_random_connection_tries = kwargs.get('max_add_random_connection_tries', 100)
         self.input_layer_nb = kwargs.get('input_layer_nb', 0)
